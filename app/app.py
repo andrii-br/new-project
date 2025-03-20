0ceb686c7f9ee6edf1b38d0b4e749024e3a8955a
Andrey Redrey <andrchoppa@gmail.com>
2025-03-20 12:32:15 +0000

    add app

---
+++ app/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, jsonify
+from models import db, Build
+
+app = Flask(name)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///builds.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    builds = Build.query.all()
+    return render_template('index.html', builds=builds)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        
+        new_build = Build(name=name, description=description)
+        db.session.add(new_build)
+        db.session.commit()
+        
+        return redirect('/')
+    return render_template('add.html')
+
+
+@app.route('/api/builds')
+def api_builds():
+    builds = Build.query.all()
+    builds_list = [{"name": build.name, "description": build.description} for build in builds]
+    return jsonify(builds_list)
+
+
+if name == 'main':
+    app.run(debug=True)
